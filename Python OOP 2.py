7d0fb17e0f9575e22aa6abe518c4755ecabe4cb0
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-26 21:47:03 +0530

    class and instance variables

--- Python OOP 2.py
+++ Python OOP 2.py
@@ -2,8 +2,8 @@
 # class variables and instance variables
 
 class Employee:
-    raise_amount = 1.04
-    num_of_emp = 0
+    raise_amount = 1.04 #class variable
+    num_of_emp = 0 #class variable
 
     def __init__(self, first, last, pay): 
         self.first = first #setting the instance variables
@@ -23,15 +23,16 @@ emp1 = Employee('John','Wick',45000) #arguments are the attributes of the object
 emp2 = Employee('Corey','Linn',50000)
 
 print(emp1.__dict__)
+emp1.raise_amount=1.05 #instance variable
+#print(emp1.__dict__) #uncomment to the changes in the emp1 dict of the variable addition into the object
 
-# emp1.raise_amount=1.05
-
-#print(Employee.raise_amount)
-print(emp1.raise_amount)
+print(Employee.raise_amount) #backend check of class variable
+print(emp1.raise_amount) #the raise_amount is changed for emp1 object since a instance variable for declared
 print(emp1.pay)
-emp1.apply_raise()
+emp1.apply_raise() #apply_raise method is called upon emp1
 print(emp1.pay)
-# print(emp2.raise_amount)
 
-# print(emp1.__dict__)
-# print(Employee.num_of_emp)
+print(emp2.raise_amount) #the emp2 object's raise_amount remains the same as the class variable since no instance variable is declared for this object i.e emp2
+
+
+print(Employee.num_of_emp) #no of employee's is printed as the number of objects are created upon the class
