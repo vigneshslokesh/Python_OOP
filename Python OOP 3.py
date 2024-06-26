ff1791e3526b2c09c6d4ecfdd460a6b8d4ea20cb
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-26 22:02:28 +0530

    different methods in python

--- Python OOP 3.py
+++ Python OOP 3.py
@@ -2,12 +2,14 @@
 
 #regular methods - takes instance as the first arg
 #class methods - takes class as the first arg
+#static methods - takes no arguments of the class or instance
+
 class Employee:
 
-    num_of_emp = 0
+    num_of_emp = 0 #class variable
     raise_amt = 1.04 #4%
 
-    def __init__(self,first,last,pay):
+    def __init__(self,first,last,pay): #initialization for the objects
         self.first = first
         self.last = last
         self.pay = pay
@@ -15,14 +17,14 @@ class Employee:
 
         Employee.num_of_emp += 1
 
-    def fullname(self):
+    def fullname(self): #regular method taking the instance as the argument i.e. self
         return "{}{}".format(self.first, self.last)
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
     @classmethod #alternating the func of a method
-    def set_raise_amt(cls, amount):
+    def set_raise_amt(cls, amount): #takes class as the first argument
         cls.raise_amt= amount
 
     @classmethod #class method as alternative constructor
@@ -32,7 +34,7 @@ class Employee:
     
     @staticmethod #static methods when no arg needed of class or instance
     def is_workday(day):
-        if day.weekday() == 5 or day.weekday() ==6:
+        if day.weekday() == 5 or day.weekday() ==6: #.weekday() is a inbuilt function
             return False
         return True
 
@@ -40,18 +42,20 @@ emp1 = Employee('Vicky', 'Koushal', 200000)
 #we use class methods as alternative constructors 
 #provides multiple ways of creating a object
 
-# emp_str1 = Employee.from_string("john-ebaon-70000")
-# print(emp_str1.email)
+emp2 = Employee.from_string("john-ebaon-70000") #in this the from_string splits the string into different variables for initializing the emp2 object
+#first it splits the string into different variable attributes of the object
+#next it passes these attributes as the arguments for initializing the objects
+print(emp2.email)
 
-#static methods = do nt pass anyrhing
+#static methods = dont pass anything
 import datetime
-my_date = datetime.date(2016, 7, 11)
+my_date = datetime.date(2016, 7, 11) #datetime library to obtain the day for a give date
 
 print(Employee.is_workday(my_date))
 print(Employee.num_of_emp) #print the number of employees
 
 
-#(The below lines can be reduced using the class methods)
+#(The below lines can be reduced using the class methods) show in the class method from_string
 # first,last,pay = emp_str1.split("-")
 # new_emp1 = Employee(first,last,pay)
 # print(new_emp1.email)
