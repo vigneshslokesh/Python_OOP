234448166fa46059c2d4af3b452bfc2f7299a3bb
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-29 00:09:32 +0530

    getter, setter and deleter

--- Python OOP 6.py
+++ Python OOP 6.py
@@ -20,10 +20,23 @@ class Employee:
     def fullname(self): #regular method taking the instance as the argument i.e. self
         return "{} {}".format(self.first, self.last)
     
+    @fullname.setter #setter
+    def fullname(self, name):
+        first , last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter #deleter
+    def fullname(self):
+       print('Delete Name!')
+       self.first = None
+       self.last = None
 
 emp1 = Employee('Jim','Smith')
-emp1.first='John'
+
+emp1.fullname='Corey Shiner'
 
 print(emp1.first)
 print(emp1.email)
-print(emp1.fullname())
+print(emp1.fullname)
+del emp1.fullname
