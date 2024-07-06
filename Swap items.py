997335df1b337a7a1c68675ae203749fd1aae52b
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-06 23:59:40 +0530

    swap items at pos

--- Swap items.py
+++ Swap items.py
@@ -8,3 +8,15 @@ input_str = input("Enter the list items: ")
 mylist = [int(item) for item in input_str.split()]
 
 print(swaplist(mylist))
+
+#swap elements at goven positions 
+
+def swapPositions(list, pos1, pos2):
+     
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+ 
+List = [23, 65, 19, 90]
+pos1, pos2  = 1, 3
+ 
+print(swapPositions(List, pos1-1, pos2-1))
